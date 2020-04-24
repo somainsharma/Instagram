051b8fadd35a06e167700d6b0ed565d4995f96cd
Somain Sharma <somainsharma@gmail.com>
2020-04-24 12:41:11 +0530

    How to call data from server is detailed in this useing ParseQuery

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -8,14 +8,17 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.GetCallback;
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView txt1, txt2,txt3;
+    private TextView fromserver ;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +29,25 @@ public class MainActivity extends AppCompatActivity {
         txt2 = findViewById(R.id.textView2);
         txt3 = findViewById(R.id.textView3);
 
+        fromserver = findViewById(R.id.textView4);
+
+        fromserver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("KickBoxer");
+                parseQuery.getInBackground("FA9DHr4Bkw", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        if(e==null && object != null){
+                            fromserver.setText(object.get("punch_speed") + " " );
+                        }else{
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
 
 
     }
@@ -33,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
     public void HelloWorldIsTapped(View view){
 
 
-   
+
 
         ParseObject kickboxer = new ParseObject("KickBoxer");
         kickboxer.put("kickspeed",txt1.toString());
