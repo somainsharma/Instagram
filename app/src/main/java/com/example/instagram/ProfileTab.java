0113e90cea850c613f86af965786e04103757fef
Somain Sharma <somainsharma@gmail.com>
2020-04-29 17:10:50 +0530

    Update two of the three fragments

--- app/src/main/java/com/example/instagram/ProfileTab.java
+++ app/src/main/java/com/example/instagram/ProfileTab.java
@@ -1,5 +1,7 @@
 package com.example.instagram;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
@@ -14,6 +24,9 @@ import android.view.ViewGroup;
  */
 public class ProfileTab extends Fragment {
 
+        private EditText edtprofilename, edtprofilebio, edtprofileprofession, edtprofilehobby,
+                            edtprofilefovsport;
+        private Button btnupdateinfo;
     public ProfileTab() {
         // Required empty public constructor
     }
@@ -23,6 +36,68 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        edtprofilename = view.findViewById(R.id.edtprofilename);
+        edtprofilebio = view.findViewById(R.id.edtbio);
+        edtprofileprofession = view.findViewById(R.id.edtprofession);
+        edtprofilehobby = view.findViewById(R.id.edthobbies);
+        edtprofilefovsport = view.findViewById(R.id.edtfovsport);
+
+        btnupdateinfo = view.findViewById(R.id.btnupdateinfo);
+
+         final ParseUser parseuser = ParseUser.getCurrentUser();
+
+    if(
+    parseuser.get("profilename") != null &&
+    parseuser.get("profilebio") != null &&
+    parseuser.get("profileprofession") != null &&
+    parseuser.get("profileshobby") != null &&
+    parseuser.get("profilefovsport") != null
+    )
+    {
+        edtprofilename.setText(parseuser.get("profilename").toString());
+        edtprofilebio.setText(parseuser.get("profilebio").toString());
+        edtprofileprofession.setText(parseuser.get("profileprofession").toString());
+        edtprofilehobby.setText(parseuser.get("profileshobby").toString());
+        edtprofilefovsport.setText(parseuser.get("profilefovsport").toString());
+    }
+
+      btnupdateinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final ProgressDialog progressDialog = new ProgressDialog(getContext());
+                progressDialog.setMessage("Updating your credentials, wait for a while !");
+                progressDialog.show();
+
+                parseuser.put("profilename", edtprofilename.getText().toString());
+                parseuser.put("profilebio", edtprofilebio.getText().toString());
+                parseuser.put("profileprofession",edtprofileprofession.getText().toString());
+                parseuser.put("profileshobby",edtprofilehobby.getText().toString());
+                parseuser.put("profilefovsport", edtprofilefovsport.getText().toString());
+
+                parseuser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e==null){
+                            FancyToast.makeText(getContext() , "Info updated successfully",
+                                    Toast.LENGTH_SHORT, FancyToast.INFO,
+                                    true).show();
+
+                        }else{
+                            FancyToast.makeText(getContext() , e.getMessage() + " ",
+                                    Toast.LENGTH_LONG, FancyToast.ERROR,
+                                    true).show();
+
+                        }
+                            progressDialog.dismiss();
+                    }
+                });
+
+            }
+        });
+        return view;
+
     }
 }
--- app/src/main/java/com/example/instagram/UsersTab.java
+++ app/src/main/java/com/example/instagram/UsersTab.java
@@ -7,12 +7,28 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UsersTab extends Fragment {
+
+    private ListView listView;
+    private ArrayList arraylist;
+    private ArrayAdapter arrayadapter;
 
     public UsersTab() {
         // Required empty public constructor
@@ -23,6 +39,41 @@ public class UsersTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_users_tab, container, false);
+
+        listView = view.findViewById(R.id.listviewusertab);
+        arraylist = new ArrayList();
+        arrayadapter = new ArrayAdapter(getContext() , android.R.layout.simple_list_item_1 , arraylist);
+        final TextView loadingusers = view.findViewById(R.id.loadingusers);
+
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+
+        parseQuery.whereNotEqualTo("username", ParseUser.getCurrentUser().getUsername());
+
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+
+                if(e == null){
+
+                    if(users.size() > 0){
+
+                        for(ParseUser user : users){
+
+                            arraylist.add(user.getUsername());
+
+                        }
+
+                        listView.setAdapter(arrayadapter);
+                        loadingusers.animate().alpha(0).setDuration(1000);
+                        listView.setVisibility(View.VISIBLE);
+                    }
+
+                }
+            }
+        });
+
+
+        return view;
     }
 }
